e792c023fce98fc76e935c97a349aebf07fd1da8
Vilasi <43801459+Vilasi@users.noreply.github.com>
2018-12-04 14:44:27 -0500

    Added Student Solution to sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+function sumAll() {
+    let arrayToCheck = Array.from(arguments);
+    let numbersToSum = [];
+    let bigNumber;
+    let smallNumber;
+    let finalSum;
 
+    for (let i = arrayToCheck.length - 1; i >= 0; i--) {
+        if (typeof arrayToCheck[i] !== "number" || arrayToCheck[i] <= 0) {
+            return "ERROR"
+        } else {
+            numbersToSum[i] = arrayToCheck[i];
+        }
+    }
+
+    bigNumber = Math.max(...numbersToSum);
+    smallNumber = Math.min(...numbersToSum);
+    finalSum = smallNumber;
+
+    for (let i = smallNumber + 1; i <= bigNumber; i++) {
+        finalSum += i
+    } 
+    return finalSum;
 }
 
 module.exports = sumAll
