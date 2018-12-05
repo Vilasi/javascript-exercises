41245bd1d5ae7bde89fb44f467c5e5965f995d70
Vilasi <43801459+Vilasi@users.noreply.github.com>
2018-12-05 00:24:48 -0500

    Student Solution

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,33 @@
-const removeFromArray = function() {
-
+function removeFromArray() {
+    let array = Array.from(arguments);
+    let arrayToSearch;
+    if (arguments.length === 1) {
+        return array[0];
+    } else {
+        arrayToSearch = [...array[0]];
+        // i is the length of the first argument, which is an array
+        // i counts down through the array's items
+        // when the value of i decrements, the j for loop runs again.
+        for (let i = arrayToSearch.length - 1; i >= 0; i--) {
+            // j is the length of the arguments
+            // j moves through the arguments until it reaches the first argument, the given array
+            for (let j = arguments.length - 1; j >= 1; j--) {
+                //as j is counted down, arguments[j] is tested to see if it equals arrayToSearch[i]
+                // arrayToSearch[i] remains on the i value of the previous loop until j counts down to 1
+                if (arrayToSearch[i] === arguments[j]) {
+                    arrayToSearch.splice(i, 1);
+                }
+            }
+        }
+        return arrayToSearch;
+    }
 }
 
+
+
+
+
+
 module.exports = removeFromArray
+
+
