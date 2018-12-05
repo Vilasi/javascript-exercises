a1f2087ff64c669a67c98ab8052a93120c074996
Vilasi <43801459+Vilasi@users.noreply.github.com>
2018-12-05 00:23:27 -0500

    Student Solution

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,39 @@
-const ftoc = function() {
+function ftoc(x) {
+let num;
 
+num = (x - 32) * (5/9);
+
+if (num % 1 === 0) {
+  num = num.toFixed(0);
+  num = Number(num);
+  return num;
+} else {
+  num = num.toFixed(1);
+  num = Number(num);
+  return num;
+}
 }
 
-const ctof = function() {
+function ctof(x) {
+let num;
 
+num = (x * (9/5)) + 32;
+
+// If remainder is 0, no decmials are required. Else, round to 1 decimal place.
+if (num % 1 === 0) {
+  num = num.toFixed(0);
+  num = Number(num);
+  return num;
+} else {
+  num = num.toFixed(1);
+  num = Number(num);
+  return num;
+}
+}
+
+module.exports = {
+  ftoc,
+  ctof
 }
 
 module.exports = {
