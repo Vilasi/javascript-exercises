cc13df54df0f39cfb7246c912935ed665eb24974
Vilasi <43801459+Vilasi@users.noreply.github.com>
2018-12-05 00:45:08 -0500

    Student Solution

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+function reverseString(a) {
+    let stringToArray = a.split('');
+    stringToArray = stringToArray.reverse();
+    stringToArray = stringToArray.join('');
+    return stringToArray;
 }
+
 
 module.exports = reverseString
