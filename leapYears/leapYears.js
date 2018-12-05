b781a4a89b8c7764c48c4b68dff77dbed2daf9ac
Vilasi <43801459+Vilasi@users.noreply.github.com>
2018-12-05 00:53:07 -0500

    Student Solution

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+function leapYears(a) {
+    if (a % 4 === 0) {
+        if (a % 100 === 0 && a % 400 !== 0) {
+            return false;
+        } else {
+            return true;
+        }
+    } else {
+        return false;
+    }
 }
 
 module.exports = leapYears
