ff084acdab5e11d3a07c8c95b36cc7b99650c421
Vilasi <43801459+Vilasi@users.noreply.github.com>
2018-12-04 23:33:35 -0500

    Repeat String Student Solution

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,21 @@
-const repeatString = function() {
-
+function repeatString(a,b) {
+    let heyString = "";
+    if (b === 1) {
+        heyString = a;
+        returnString = heyString;
+    } else if (b === 0) {
+        returnString = heyString;
+    } else if (b < 0) {
+        heyString = "ERROR";
+        returnString = heyString;
+    } else {
+        for (let i = 0; i < b; i++) {
+            heyString += a;
+            returnString = heyString;
+        }
+    }
+    return returnString;
 }
 
-module.exports = repeatString
+
+   module.exports = repeatString
